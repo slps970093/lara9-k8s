37716482bc8bbce56143538b0442bacd987b735e
小周 <slps970093gmail.com>
2024-04-26 11:37:26 +0800

    T

--- cdk8s/main.ts
+++ cdk8s/main.ts
@@ -75,38 +75,15 @@ server {
     gzip_disable "MSIE [1-6]\\.";
 
     location / {
-        # First attempt to serve request as file, then
-        # as directory, then fall back to index.php
-        # try_files $uri $uri/ /index.php?$query_string $uri/index.html;
-
-        try_files $uri $uri/ /index.php?$query_string;
-    }
-
-    # redirect server error pages to the static page /50x.html
-    #
-    error_page   500 502 503 504  /50x.html;
-    location = /50x.html {
-        root   /usr/share/nginx/html;
-    }
-
-    # pass the PHP scripts to FastCGI server listening on socket
-    #
-    location ~ \\.php$ {
-        try_files $uri $uri/ /index.php?$query_string;
-        fastcgi_split_path_info ^(.+\\.php)(/.+)$;
+        # PHP FPM 所在位置
+        root           /var/www/html/public;
         fastcgi_pass ${phpFpmCfg.serviceName}.${phpFpmCfg.namespace}.svc.cluster.local:9000;
         fastcgi_index index.php;
         include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME /var/www/html/public$fastcgi_script_name;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
-
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         fastcgi_read_timeout 600s;
         fastcgi_buffering off;
     }
-
-        location ~* \\.(jpg|jpeg|gif|png|css|js|ico|xml)$ {
-                expires           5d;
-        }
 
     # deny access to . files, for security
     #
