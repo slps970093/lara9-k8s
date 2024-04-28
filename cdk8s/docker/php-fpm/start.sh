6a8c5b963789a9f3a68241f32a03d1811d18ed5c
小周 <slps970093gmail.com>
2024-04-29 00:52:35 +0800

    T

--- cdk8s/docker/php-fpm/start.sh
+++ cdk8s/docker/php-fpm/start.sh
@@ -4,4 +4,7 @@ chown -Rf www-data:www-data /var/www/html/storage
 chown -Rf www-data:www-data /var/www/html/bootstrap
 chown -Rf www-data:www-data /var/www/html/public
 
+mkdir /var/log/php
+chmod 666 /var/log/php
+
 supervisord -n -c /var/www/html/cdk8s/docker/php-fpm/supervisord.conf
